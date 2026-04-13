aefbc30e6f63ace429585d2e1af5368e32f42e02
Anwar Hossain <cseanwar@gmail.com>
2026-04-13 18:57:11 +0200

    Completed Navbar with active buttons/page

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import logo from "../../../assets/logo.png"
+import logo from "../../../assets/logo.png";
 import { RiHome2Line } from "react-icons/ri";
 import { PiChartLineBold } from "react-icons/pi";
 import { LuClock3 } from "react-icons/lu";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
@@ -12,9 +13,41 @@ const Navbar = () => {
         </div>
 
         <div className="navbar-end">
-          <a className="btn border-0 bg-[#244D3F] text-[#FFFFFF] font-semibold text-base"><RiHome2Line />Home</a>
-          <a className="btn border-0 bg-[#FFFFFF] text-[#64748B] font-semibold text-base"><LuClock3 />Timeline</a>
-          <a className="btn border-0 bg-[#FFFFFF] text-[#64748B] font-semibold text-base"><PiChartLineBold />Stats</a>
+          <a>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `btn font-semibold text-base border-0 ${isActive ? "text-[#FFFFFF] bg-[#244D3F]" : "bg-[#FFFFFF] text-[#64748B]"}`
+              }
+            >
+              <RiHome2Line />
+              Home
+            </NavLink>
+          </a>
+
+          <a>
+            <NavLink
+              to={"/timeline"}
+              className={({ isActive }) =>
+                `btn font-semibold text-base border-0 ${isActive ? "text-[#FFFFFF] bg-[#244D3F]" : "bg-[#FFFFFF] text-[#64748B]"}`
+              }
+            >
+              <LuClock3 />
+              Timeline
+            </NavLink>
+          </a>
+
+          <a>
+            <NavLink
+              to={"/stats"}
+              className={({ isActive }) =>
+                `btn font-semibold text-base border-0 ${isActive ? "text-[#FFFFFF] bg-[#244D3F]" : "bg-[#FFFFFF] text-[#64748B]"}`
+              }
+            >
+              <PiChartLineBold />
+              Stats
+            </NavLink>
+          </a>
         </div>
       </div>
     </div>
