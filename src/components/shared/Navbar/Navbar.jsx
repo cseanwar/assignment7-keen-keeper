b5843cc81396e98a9f47d2f4a7e46d411761bf56
Anwar Hossain <cseanwar@gmail.com>
2026-04-13 18:16:19 +0200

    Added Navbar and google fonts

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from "react";
 import logo from "../../../assets/logo.png"
+import { RiHome2Line } from "react-icons/ri";
+import { PiChartLineBold } from "react-icons/pi";
+import { LuClock3 } from "react-icons/lu";
 
 const Navbar = () => {
   return (
@@ -10,7 +12,9 @@ const Navbar = () => {
         </div>
 
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn border-0 bg-[#244D3F] text-[#FFFFFF] font-semibold text-base"><RiHome2Line />Home</a>
+          <a className="btn border-0 bg-[#FFFFFF] text-[#64748B] font-semibold text-base"><LuClock3 />Timeline</a>
+          <a className="btn border-0 bg-[#FFFFFF] text-[#64748B] font-semibold text-base"><PiChartLineBold />Stats</a>
         </div>
       </div>
     </div>
