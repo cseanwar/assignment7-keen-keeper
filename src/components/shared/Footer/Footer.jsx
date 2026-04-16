0f1b7cc00fdc25e980dafc329d8ff90689f81e08
Anwar Hossain <cseanwar@gmail.com>
2026-04-17 00:16:45 +0200

    Make footer responsive

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -6,9 +6,9 @@ import twitterImg from "../../../assets/twitter.png";
 const Footer = () => {
   return (
     <div className="bg-[#244D3F]">
-      <div className="w-9/12 mx-auto pt-20">
-        <div className="flex flex-col gap-4 justify-center items-center text-center pb-10">
-          <img src={footerImg} alt="" />
+      <div className="w-11/12 md:w-10/12 lg:w-9/12 mx-auto pt-12 md:pt-16 lg:pt-20">
+        <div className="flex flex-col gap-4 justify-center items-center text-center pb-8 md:pb-10">
+          <img src={footerImg} alt="Friend Keeper logo" />
           <p className="text-[#FFFFFF80] text-base pb-4">
             Your personal shelf of meaningful connections. Browse, tend, and
             nurture the relationships that matter most.
@@ -28,10 +28,12 @@ const Footer = () => {
             </div>
           </div>
         </div>
+
         <div className="border-t border-[#1A886240]"></div>
-        <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-left py-7.5 gap-6 md:gap-0">
-            <p className="text-[#FAFAFA50] text-base order-2 md:order-1">© 2026 KeenKeeper. All rights reserved.</p>
-            <div className="flex flex-wrap justify-center gap-6 md:gap-10 text-[#FAFAFA50] text-base order-1 md:order-2">
+
+        <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-left py-6 md:py-7 gap-4 md:gap-0">
+            <p className="text-[#FAFAFA50] text-xs md:text-sm lg:text-base order-2 md:order-1">© 2026 KeenKeeper. All rights reserved.</p>
+            <div className="flex flex-wrap justify-center gap-4 md:gap-6 lg:gap-10 text-[#FAFAFA50] text-xs md:text-sm lg:text-base order-1 md:order-2 cursor-pointer">
                 <p>Privacy Policy</p>
                 <p>Terms of service</p>
                 <p>Cookies</p>
